fd5ca3757a83fa784155f8af6efd064809bae976
Cathleen Melendez <cathleen@melendez.com>
2018-08-14 21:21:28 +0000

    Added validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+@store_name = gets.chomp
+@store = Store.create(name: @store_name)
+@store.errors.messages.each do |message|
+  puts message
+end
