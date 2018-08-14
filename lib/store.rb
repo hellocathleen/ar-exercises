99af160768b992da8bd45739c1ae489479859e35
Cathleen Melendez <cathleen@melendez.com>
2018-08-14 23:50:30 +0000

    Added callbacks

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  before_destroy :check_employees
+
+  private
+
+  def check_employees
+    if employees.count >= 1
+      return false
+    end
+  end
 end
